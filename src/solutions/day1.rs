33a010a0114175eb6f734e69c50f8fdf80a6dfc0
Jamb000h <8905786+Jamb000h@users.noreply.github.com>
2022-12-01 23:48:30 +0200

    Day 1 (Rust)

---
+++ src/solutions/day1.rs
@@ -0,0 +1,44 @@
+pub mod solution {
+
+    pub fn solve() {
+        let input = read_and_parse_input();
+        let elves = wrangle_input(input);
+        self::part_1(&elves);
+        self::part_2(&elves);
+    }
+
+    fn read_and_parse_input() -> Vec<String> {
+        crate::utils::read_operations::read_to_lines("./src/inputs/day1.txt")
+    }
+
+    fn wrangle_input(input: Vec<String>) -> Vec<Vec<i32>> {
+        let mut elves = Vec::new();
+        let mut elf = Vec::new();
+        for row in input {
+            if row.len() == 0 {
+                elves.push(elf);
+                elf = Vec::new();
+            } else {
+                elf.push(row.parse::<i32>().unwrap())
+            }
+        }
+        elves.push(elf);
+        elves
+    }
+
+    fn part_1(elves: &Vec<Vec<i32>>) {
+        let largest: i32 = elves
+            .iter()
+            .map(|elf| elf.iter().sum::<i32>())
+            .max()
+            .unwrap();
+        println!("Day 1 part 1: {}", largest)
+    }
+
+    fn part_2(elves: &Vec<Vec<i32>>) {
+        let mut largest: Vec<i32> = elves.iter().map(|elf| elf.iter().sum::<i32>()).collect();
+        largest.sort();
+        let top_three_sum: i32 = largest.iter().rev().take(3).sum();
+        println!("Day 1 part 2: {}", top_three_sum)
+    }
+}
---
+++ src/utils/mod.rs
@@ -0,0 +1,25 @@
+pub mod read_operations {
+    pub fn read_to_vec_i32(file_path: &str) -> Vec<i32> {
+        std::fs::read_to_string(file_path)
+            .unwrap()
+            .lines()
+            .map(|row| row.parse::<i32>().unwrap())
+            .collect()
+    }
+
+    pub fn read_to_vec_i64(file_path: &str) -> Vec<i64> {
+        std::fs::read_to_string(file_path)
+            .unwrap()
+            .lines()
+            .map(|row| row.parse::<i64>().unwrap())
+            .collect()
+    }
+
+    pub fn read_to_lines(file_path: &str) -> Vec<String> {
+        std::fs::read_to_string(file_path)
+            .unwrap()
+            .lines()
+            .map(|row| row.to_owned())
+            .collect()
+    }
+}
